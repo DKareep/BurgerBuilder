aefe47e7fb9ea2b7ccb9c4ef43a6bd12094d48d6
dijin <kareep7@gmail.com>
2018-06-10 20:34:58 +0530

    more tests

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ yarn install
 npm run start
 
 
-Demo burger builder project is built using React, Redux and react-router,axios etc.This project has a Token based Auth system with 1hr session. Authentication is done using Firebase auth API. Users can register/login, build burger of their choice and also check orders made by the that user.
+Demo burger builder project is built using React, Redux and react-router,axios, enzyme(unit test) etc.This project has a Token based Auth system with 1hr session. Authentication is done using Firebase auth API. Users can register/login, build burger of their choice and also check orders made by the that user.
 
 -----------
 
--- src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
-import Navigation from './NavigationItem/NavigationItem';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 
 configure({adapter: new Adapter()})
@@ -10,10 +10,15 @@ configure({adapter: new Adapter()})
 describe('<NavigationItems />', ()=> {
     it('Should render one <NavigationItem /> elements if not authenticated',()=>{
             const wrapper = shallow(<NavigationItems />)
-            expect(wrapper.find(Navigation)).toHaveLength(1)
+            expect(wrapper.find(NavigationItem)).toHaveLength(1)
     })
     it('Should render one <NavigationItem /> elements if  authenticated',()=>{
-        const wrapper = shallow(<NavigationItems isAuth/>)
-        expect(wrapper.find(Navigation)).toHaveLength(3)
+        const wrapper = shallow(<NavigationItems />)
+        wrapper.setProps({isAuth: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+})
+it('Should contain <NavigationItem /> elements if authenticated',()=>{
+    const wrapper = shallow(<NavigationItems isAuth />)
+    expect(wrapper.contains(<NavigationItem link="/logout" >Logout</NavigationItem>)).toEqual(true)
 })
 })
